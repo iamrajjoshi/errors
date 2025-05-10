89b2128ea10f70ba61aebd3235b097e9e510282f
Raj Joshi <raj.joshi@sentry.io>
2025-05-10 14:45:58 -0700

    :sparkles: feat: add posthog analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,25 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
+import { PostHogProvider } from './providers';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html lang="en">
-      <head>
-        <title>Sentry Error Generator</title>
-        <meta name="description" content="Generate and send test errors to your Sentry project" />
-      </head>
-      <body>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <title>Sentry Error Generator</title>
+                <meta
+                    name="description"
+                    content="Generate and send test errors to your Sentry project"
+                />
+            </head>
+            <body>
+                <PostHogProvider>
+                    <ChakraProvider theme={theme}>{children}</ChakraProvider>
+                </PostHogProvider>
+            </body>
+        </html>
+    );
 };
 
 export default RootLayout;
